5bb2082f53a39c1c66a8f4f2215a98b58d40000f
NILOY ROY <121684663+xanon-oc@users.noreply.github.com>
2023-05-12 22:30:31 +0600

    added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const app = express();
 const port = process.env.PORT || 5000;
 
